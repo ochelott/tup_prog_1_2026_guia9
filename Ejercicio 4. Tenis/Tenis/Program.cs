f6d3848a35abc33587d39b19f0d2949f97160e10
ochelott <benjash8@outlook.com>
2026-06-03 23:18:29 -0300

    Ejercicio 4. Tenis

---
+++ Ejercicio 4. Tenis/Tenis/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tenis
+{
+    internal class Program
+    {
+        static Servicio servicio = new Servicio();
+
+        static string DeterminarGanador()
+        {
+            Console.Clear();
+            Console.WriteLine("El ganador es: " + servicio.DeterminarGanador());
+            return servicio.ToString();
+        }
+        static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.WriteLine("Seleccione una opción:");
+            Console.WriteLine("1. Registrar los nombres de los jugadores.");
+            Console.WriteLine("2. Registrar los resultados de cada set de los jugadores.");
+            Console.WriteLine("3. Mostrar el ganador.");
+            Console.WriteLine("4. Salir");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            return opcion;
+            
+        }
+        static void MostrarPantallaSolicitarNombreJugadores() 
+        {
+
+            Console.WriteLine("Jugador 1: ");
+            string nombre1 = Console.ReadLine();
+
+            Console.WriteLine("Jugador 2: ");
+            string nombre2 = Console.ReadLine();
+
+            servicio.RegistrarJugadores(nombre1, nombre2);
+            Console.Clear();
+        }
+        static void MostrarPantallaSolicitarResultadoSet()
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($"Set {i} jugador 1:");
+                int resultado1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"Set {i} jugador 2:");
+                int resultado2 = Convert.ToInt32(Console.ReadLine());
+
+                servicio.RegistrarResultadoSet(resultado1, resultado2);
+            }
+            Console.Clear();
+        }
+        static void MostrarPantallaGanador()
+        {
+            Console.Clear();
+            Console.WriteLine("El ganador es: " + servicio.DeterminarGanador());
+        }
+        static void Main(string[] args)
+        {
+            int opcion;
+            do
+            {
+                opcion = MostrarPantallaSolicitarOpcionMenu();
+                Console.Clear();
+                switch (opcion)
+                {
+                    case 1:
+                        MostrarPantallaSolicitarNombreJugadores();
+                        break;
+                    case 2:
+                        MostrarPantallaSolicitarResultadoSet();
+                        break;
+                    case 3:
+                        MostrarPantallaGanador();
+                        break;
+                    case 4:
+                        Console.WriteLine("Saliendo del programa...");
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida. Por favor, seleccione una opción del menú.");
+                        break;
+                }
+                
+                Console.WriteLine();
+                
+            } while (opcion != 4);
+                
+            Console.WriteLine("Programa finalizado. Presione cualquier tecla para salir.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio 4. Tenis/Tenis/Servicio.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tenis
+{
+    internal class Servicio
+    {
+        public string Jugador1;
+        private int setGanados1 = 0;
+        public string Jugador2;
+        private int setGanados2 = 0;
+        
+        public void RegistrarJugadores(string Nombre1, string Nombre2)
+        {
+            Jugador1 = Nombre1;
+            Jugador2 = Nombre2;
+        }
+        public void RegistrarResultadoSet(int resultado1, int resultado2)
+        {
+          if (resultado1 > resultado2)
+            {
+                setGanados1++;
+            }
+            else if (resultado2 > resultado1)
+            {
+                setGanados2++;
+            }
+
+        }
+            public string DeterminarGanador()
+        {
+            if (setGanados1 > setGanados2)
+            {
+                return Jugador1;
+            }
+            else if (setGanados2 > setGanados1)
+            {
+                return Jugador2;
+            }
+            else
+            {
+                return "El partido está empatado.";
+            }
+        }
+    }
+}
