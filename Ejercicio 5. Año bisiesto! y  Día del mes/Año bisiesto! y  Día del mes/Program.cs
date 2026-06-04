fe4e0c754a9fe2220c044811f7a663c64c743544
ochelott <benjash8@outlook.com>
2026-06-04 00:46:53 -0300

    Ejercicio 5 año bisiesto

---
+++ Ejercicio 5. Año bisiesto! y  Día del mes/Año bisiesto! y  Día del mes/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Año_bisiesto__y__Día_del_mes
+{
+    internal class Program
+    {
+        static Servicio servicio = new Servicio();
+
+        static int SolicitarPantallaOpcionMenu()
+        {
+            Console.WriteLine("Seleccione una opción: ");
+            Console.WriteLine("1. Determinar los días del mes. ");
+            Console.WriteLine("2. Verificar si un año es bisiesto. ");
+            Console.WriteLine("3. Salir");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            return opcion;
+        }
+        static void MostrarPantallaSolicitarMesAñoYDeterminarDias()
+        {
+            Console.Clear();
+            
+            Console.WriteLine("Ingrese el mes (1-12): ");
+            int mes = Convert.ToInt32(Console.ReadLine());
+            if (mes >= 1 && mes <= 12)
+            {
+                Console.WriteLine("Ingrese el año: ");
+                int año = Convert.ToInt32(Console.ReadLine());
+                int dias = servicio.DeterminarLosDiasDelMes(mes, año);
+                Console.WriteLine($"El mes {mes} del año {año} tiene {dias} días.");
+            } else Console.WriteLine("Mes no válido. Por favor, ingrese un número entre 1 y 12.");
+
+        }
+        static void MostrarPantallaVerificarSiElAñoEsBisiesto()
+        {
+            Console.Clear();
+            Console.WriteLine("Ingrese el año: ");
+            int año = Convert.ToInt32(Console.ReadLine());
+            if (servicio.EsBisiesto(año))
+            {
+                Console.WriteLine($"El año {año} es bisiesto.");
+            }
+            else
+            {
+                Console.WriteLine($"El año {año} no es bisiesto.");
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            int opcion;
+
+            do
+            {
+                opcion = SolicitarPantallaOpcionMenu();
+                Console.Clear();
+                
+                switch (opcion)
+                {
+                    case 1:
+                        MostrarPantallaSolicitarMesAñoYDeterminarDias();
+                        break;
+                    case 2:
+                        MostrarPantallaVerificarSiElAñoEsBisiesto();
+                        break;
+                    case 3:
+                        Console.WriteLine("Saliendo del programa...");
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida. Por favor, seleccione una opción del menú.");
+                        break;
+                }
+                
+                Console.WriteLine();
+
+            } while (opcion != 3);
+
+             Console.WriteLine("Presione cualquier tecla para salir...");
+            Console.ReadKey(); 
+
+        }
+    }
+}
---
+++ Ejercicio 5. Año bisiesto! y  Día del mes/Año bisiesto! y  Día del mes/Servicio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Año_bisiesto__y__Día_del_mes
+{
+    internal class Servicio
+    {
+        public int DeterminarLosDiasDelMes(int mes, int año)
+        {
+            int dias = 0;
+            switch (mes)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    dias = 31;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    dias = 30;
+                    break;
+                case 2:
+                    if ((año % 4 == 0 && año % 100 != 0) || (año % 400 == 0))
+                    {
+                        dias = 29;
+                    }
+                    else
+                    {
+                        dias = 28;
+                    }
+                    break;
+                
+            }
+            return dias;
+        }
+        public bool EsBisiesto(int año)
+        {
+            return (año % 4 == 0 && año % 100 != 0) || (año % 400 == 0);
+        }
+    }
+}
