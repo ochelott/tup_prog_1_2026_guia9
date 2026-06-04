e27a3b9b174b4dc4cd3eaa3591dcafd6df7b7646
ochelott <benjash8@outlook.com>
2026-06-03 21:39:46 -0300

    Ejercicio 2. Las niñas

---
+++ Ejercicio 2. Las niñas/Las niñas/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Las_niñas
+{
+    internal class Program
+    {
+        static Servicio Servicio = new Servicio();
+
+        static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.WriteLine("Seleccione una opción:");
+            Console.WriteLine("1. Registrar monto a repartir");
+            Console.WriteLine("2. Registrar edades de las niñas");
+            Console.WriteLine("3. Calcular montos y porcentajes");
+            Console.WriteLine("4. Salir");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            return opcion;
+        }   
+        static void MostrarPantallaSolicitarMontoARepartir()
+        {
+            Console.Clear();
+            Console.WriteLine("Ingrese el monto a repartir:");
+            double monto = Convert.ToDouble(Console.ReadLine());
+            Servicio.RegistrarMontoARepartir(monto);
+
+        }
+        static void MostrarPantallaRegistrarEdadesDeLasNiñas()
+        {
+            Console.Clear();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Ingrese la edad de la niña {i}:");
+                int edad = Convert.ToInt32(Console.ReadLine());
+                if (edad < 0)
+                {
+                    Console.WriteLine("La edad no puede ser negativa. Intente nuevamente.");
+                    i--; // Decrementar para volver a solicitar la edad de la misma niña
+                    continue;
+                }
+                Servicio.RegistrarEdad(edad, i);
+            }
+        }
+        static void MostrarPantallaCalcularMostrarMontoYPorcentajePorNiña()
+        {
+            Console.Clear();
+            Servicio.CalcularMontosYPorcentajesARepartir();
+            if (Servicio.Monto <= 0)
+            {
+                Console.WriteLine("No se han registrado las edades de las niñas. Por favor, registre las edades antes de calcular los montos y porcentajes.");
+                return;
+            }
+            Console.WriteLine($"Niña 0: Monto = $ {Servicio.Monto0:F2} / Porcentaje = {Servicio.Porcentaje0:F2}%");
+            Console.WriteLine($"Niña 1: Monto = $ {Servicio.Monto1:F2} / Porcentaje = {Servicio.Porcentaje1:F2}%");
+            Console.WriteLine($"Niña 2: Monto = $ {Servicio.Monto2:F2} / Porcentaje = {Servicio.Porcentaje2:F2}%");
+            Console.WriteLine($"Niña 3: Monto = $ {Servicio.Monto3:F2} / Porcentaje = {Servicio.Porcentaje3:F2}%");
+        }
+
+        static void Main(string[] args)
+        {
+            int opcion;
+
+            do
+            {
+                opcion = MostrarPantallaSolicitarOpcionMenu();
+                switch (opcion)
+                {
+                    case 1:
+                        MostrarPantallaSolicitarMontoARepartir();
+                        break;
+                    case 2:
+                        MostrarPantallaRegistrarEdadesDeLasNiñas();
+                        break;
+                    case 3:
+                        MostrarPantallaCalcularMostrarMontoYPorcentajePorNiña();
+                        break;
+                    case 4:
+                        Console.WriteLine("Saliendo del programa...");
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida. Intente nuevamente.");
+                        break;
+                }
+                Console.WriteLine();
+            } while (opcion != 4);
+            
+            Console.WriteLine("Programa finalizado. Presione cualquier tecla para salir.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio 2. Las niñas/Las niñas/Servicio.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Las_niñas
+{
+    internal class Servicio
+    {
+        public int Edad0;
+        public int Edad1;
+        public int Edad2;
+        public int Edad3;
+
+        public double Monto;
+        public double Porcentaje0;
+        public double Porcentaje1;
+        public double Porcentaje2;
+        public double Porcentaje3;
+        public double Monto0;
+        public double Monto1;
+        public double Monto2;
+        public double Monto3;
+    
+    public void RegistrarMontoARepartir(double monto)
+        {
+            if (monto <= 0)
+            {
+                Console.WriteLine("El monto debe ser un número positivo. Intente nuevamente.");
+                return;
+            }
+            Monto = monto;
+        }
+        public void RegistrarEdad(int edad, int nroNiña)
+        {
+            switch (nroNiña)
+            {
+                case 0:
+                    Edad0 = edad;
+                    break;
+                case 1:
+                    Edad1 = edad;
+                    break;
+                case 2:
+                    Edad2 = edad;
+                    break;
+                case 3:
+                    Edad3 = edad;
+                    break;
+            }
+        }
+        public void CalcularMontosYPorcentajesARepartir()
+        {
+            int suma = Edad0 + Edad1 + Edad2 + Edad3;
+            if (suma > 0)
+            {
+                Porcentaje0 = ((double)Edad0 / suma) * 100;
+                Porcentaje1 = ((double)Edad1 / suma) * 100;
+                Porcentaje2 = ((double)Edad2 / suma) * 100;
+                Porcentaje3 = ((double)Edad3 / suma) * 100;
+                Monto0 = (Porcentaje0 * Monto) / 100;
+                Monto1 = (Porcentaje1 * Monto) / 100;
+                Monto2 = (Porcentaje2 * Monto) / 100;
+                Monto3 = (Porcentaje3 * Monto) / 100;
+            }
+        }
+    }
+}
