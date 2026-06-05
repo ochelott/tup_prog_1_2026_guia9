88000d1705527e80323716cc2feca3ea6d60459b
ochelott <benjash8@outlook.com>
2026-06-05 16:08:46 -0300

    Ejercicio 7. Rubros

---
+++ Ejercicio 7. Rubros/Rubros/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rubros
+{
+    internal class Program
+    {
+
+        static servicio servicio = new servicio();
+        static int MostrarPantallaOpcionMenu()
+        {
+            Console.WriteLine("Ingrese una opcion: ");
+            Console.WriteLine("1.Ingresar un resumen de venta");
+            Console.WriteLine("2.Mostrar Numero de transaccion registrado con el mayor monto total.");
+            Console.WriteLine("3.Mostrar porcentaje de cantidad de ventas por rubro.");
+            Console.WriteLine("4.Mostrar recaudacion total.");
+            Console.WriteLine("5.Salir");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            return opcion;
+        }
+        static void MostrarPantallaRegistrarTransaccion()
+        {
+            
+            Console.WriteLine("Ingrese el numero de transaccion: ");
+            int nroTransaccion = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el rubro: [1 - 5] ");
+            int rubro = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad:");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el monto:");
+            double monto = Convert.ToDouble(Console.ReadLine());
+            servicio.EvaluarTransaccionPuntoDeVenta(nroTransaccion, rubro, cantidad, monto);
+            Console.Clear();
+        }
+        static void MostrarPantallaPorcentajeDeCantidadesPorRubro()
+        {
+            Console.Clear();
+            servicio.CalcularPorcentajesCantidadVentasPorRubro();
+            Console.WriteLine($"Rubro 1: {servicio.PorcentajeCantidadRubro1:F2} %");
+            Console.WriteLine($"Rubro 2: {servicio.PorcentajeCantidadRubro2:F2} %");
+            Console.WriteLine($"Rubro 3: {servicio.PorcentajeCantidadRubro3:F2} %");
+            Console.WriteLine($"Rubro 4: {servicio.PorcentajeCantidadRubro4:F2} %");
+            Console.WriteLine($"Rubro 5: {servicio.PorcentajeCantidadRubro5:F2} %");
+        }
+        static void MostrarPantallaTransaccionMayorMonto()
+        {
+            Console.Clear();
+            Console.WriteLine("Numero de transaccion: " + servicio.NumeroTransaccionMayor);
+            Console.WriteLine("Monto Mayor: " + servicio.MontoTransaccionMayor);
+        }
+        static void MostrarPantallaMontoRecaudadoTotal()
+        {
+            Console.Clear();
+            Console.WriteLine($"Recaudacion total: ${servicio.RecaudacionTotal:F2}");
+        }
+        static void Main(string[] args)
+        {
+
+            servicio.InicializarVariables();
+            int opcion;
+
+            do
+            {
+                opcion = MostrarPantallaOpcionMenu();
+                Console.Clear();
+
+                switch (opcion)
+                {
+                    case 1:
+                        MostrarPantallaRegistrarTransaccion();
+                        break;
+                    case 2:
+                        MostrarPantallaTransaccionMayorMonto();
+                        break;
+                    case 3:
+                        MostrarPantallaPorcentajeDeCantidadesPorRubro();
+                        break;
+                    case 4:
+                        MostrarPantallaMontoRecaudadoTotal();
+                        break;
+                    case 5:
+                        Console.WriteLine("Saliendo del programa...");
+                        break;
+                    default:
+                        Console.WriteLine("Opcion invalida..");
+                        break;
+                }
+            } while (opcion != 5);
+            Console.WriteLine("Presione cualquier tecla para salir.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio 7. Rubros/Rubros/servicio.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rubros
+{
+    internal class servicio
+    {
+        private int cantidad1;
+        private int cantidad2;
+        private int cantidad3;
+        private int cantidad4;
+        private int cantidad5;
+        public int NumeroTransaccionMayor;
+        public double MontoTransaccionMayor;
+        private int contadorDeTransacciones;
+        public double PorcentajeCantidadRubro1;
+        public double PorcentajeCantidadRubro2;
+        public double PorcentajeCantidadRubro3;
+        public double PorcentajeCantidadRubro4;
+        public double PorcentajeCantidadRubro5;
+        public double RecaudacionTotal;
+
+        public void InicializarVariables()
+        {
+            cantidad1 = 0;
+            cantidad2 = 0;
+            cantidad3 = 0;
+            cantidad4 = 0;
+            cantidad5 = 0;
+            NumeroTransaccionMayor = 0;
+            contadorDeTransacciones = 1;
+            MontoTransaccionMayor = 0;
+            PorcentajeCantidadRubro1 = 0;
+            PorcentajeCantidadRubro2 = 0;
+            PorcentajeCantidadRubro3 = 0;
+            PorcentajeCantidadRubro4 = 0;
+            PorcentajeCantidadRubro5 = 0;
+            RecaudacionTotal = 0;
+        }
+        public void EvaluarTransaccionPuntoDeVenta(int nroTransaccion, int rubro, int cantidad, double monto)
+        {
+            switch (rubro)
+            {
+                case 1:
+                    cantidad1 += cantidad;
+                    break;
+                case 2:
+                    cantidad2 += cantidad;
+                    break;
+                case 3:
+                    cantidad3 += cantidad;
+                    break;
+                case 4:
+                    cantidad4 += cantidad;
+                    break;
+                case 5:
+                    cantidad5 += cantidad;
+                    break;
+                default:
+                    Console.WriteLine("Opcion invalida.");
+                    Console.WriteLine("Vuelva a ingresar todos los datos nuevamente..");
+                    break;
+            }
+            if (monto > MontoTransaccionMayor)
+            {
+                MontoTransaccionMayor = monto;
+                NumeroTransaccionMayor = nroTransaccion;
+            }
+            RecaudacionTotal += monto;
+            contadorDeTransacciones++;
+        }
+        public void CalcularPorcentajesCantidadVentasPorRubro()
+        {
+
+            int totalCantidad = cantidad1 + cantidad2 + cantidad3 + cantidad4 + cantidad5;
+            if (totalCantidad > 0)
+            {
+                PorcentajeCantidadRubro1 = (double)cantidad1 / totalCantidad * 100;
+                PorcentajeCantidadRubro2 = (double)cantidad2 / totalCantidad * 100;
+                PorcentajeCantidadRubro3 = (double)cantidad3 / totalCantidad * 100;
+                PorcentajeCantidadRubro4 = (double)cantidad4 / totalCantidad * 100;
+                PorcentajeCantidadRubro5 = (double)cantidad5 / totalCantidad * 100;
+            }
+            else Console.WriteLine("No se ingreso ninguna venta..");
+        }
+    }
+}
